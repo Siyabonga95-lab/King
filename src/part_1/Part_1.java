86e863d6e6f34844ea0c11d08c9e7e277c183697
Siyabonga <Siyabonga@TheKing.home>
2025-03-23 20:59:30 +0200

    PART_1

---
+++ src/part_1/Part_1.java
@@ -0,0 +1,19 @@
+
+package part_1;
+
+/**
+ *Student Name: Siyabonga Msimango
+ *Student Number: ST10488360
+ * Assignment Part 1
+ */
+public class Part_1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Registration registration = new Registration();
+      //Login login = new Login(registration;
+     registration.setVisible(true);
+    }
+}
